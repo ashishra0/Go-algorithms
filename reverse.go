f235544c8d7cbcebab24fa439cc0aaaac3a7e284
Ashish Rao <ashishrao2598@gmail.com>
2019-08-05 13:09:04 +0530

    add new reverse method

--- reverse.go
+++ reverse.go
@@ -1,9 +1,17 @@
 package main
 
-func reverseStr(str string) string {
+func reversePrune(str string) string {
 	rev := []rune(str)
 	for i, j := 0, len(rev)-1; i < j; i, j = i+1, j-1 {
 		rev[i], rev[j] = rev[j], rev[i]
 	}
 	return string(rev)
+}
+
+func reverse(str string) string {
+	var rev string
+	for i := len(str) - 1; i >= 0; i-- {
+		rev += string(str[i])
+	}
+	return rev
 }
