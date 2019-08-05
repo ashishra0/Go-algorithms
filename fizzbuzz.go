ea490046c23b0f203ad1c351bfacdf3cbfa899f5
Ashish Rao <ashishrao2598@gmail.com>
2019-08-05 12:55:36 +0530

    add fizzbuzz program

---
+++ fizzbuzz.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func fizzBuzz(n int) {
+	for i := 1; i <= n; i++ {
+		if i%5 == 0 && i%3 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
